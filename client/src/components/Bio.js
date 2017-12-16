044252c4f713730f5e5f3a60effb03de5fb8682b
Justin Dean <deanplums@gmail.com>
2017-12-16 16:47:18 -0700

    berz picture

--- client/src/components/Bio.js
+++ client/src/components/Bio.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
-import { Segment, Header } from 'semantic-ui-react'
+import { Segment, Header, Image, Card, Icon } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import Berz from '../images/matthew.png';
 
 const Bio = () => (
-  <Segment basic> 
-    <Header> Super Awesome Bio </Header> 
-  </Segment> 
+ <Segment basic>
+   <Header> Super Awesome Bio </Header>
+   <Card>
+   <Image src={Berz} />
+   <Card.Content>
+     <Card.Header>
+       Berz
+     </Card.Header>
+     <Card.Meta>
+       <span className='date'>
+         Joined in 2015
+       </span>
+     </Card.Meta>
+     <Card.Description>
+       Berz is a musician living in Nashville. Hairstyle by Jesus.
+     </Card.Description>
+   </Card.Content>
+   <Card.Content extra>
+     <a>
+       <Icon name='user' />
+       22 Friends
+     </a>
+   </Card.Content>
+ </Card>
+ </Segment> 
 )
 
 export default Bio;
